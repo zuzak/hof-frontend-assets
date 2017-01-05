75fd3b1e4ccd08637be786a9592f6adc1c5e1318
JoeChapman <joseph.philip.chapman@gmail.com>
2017-01-05 11:48:34 +0000

    use witch to find node-sass module

--- bin/tasks.js
+++ bin/tasks.js
@@ -4,11 +4,12 @@ const path = require('path');
 const cwd = process.cwd();
 const target = path.resolve(cwd, 'public');
 const source = path.resolve(__dirname, '../src');
+const sass = require('witch')('node-sass');
 
 // jscs:disable maximumLineLength
 module.exports = {
   'make-folders': `mkdir -p ${target}/js ${target}/css ${target}/images`,
-  'compile-css': `${cwd}/node_modules/.bin/node-sass ${source}/scss/app.scss ${target}/css/app.css --include-path ./node_modules`,
+  'compile-css': `${sass} ${source}/scss/app.scss ${target}/css/app.css --include-path ./node_modules`,
   'bundle-js': `browserify ${source}/js/index.js > ${target}/js/bundle.js`,
   'copy-images': `cp -r ${source}/images ${target}`
 };
